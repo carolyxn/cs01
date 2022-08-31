625f4085b42763a3528ace736ebc7a72bb46a6eb
carolyxn <112140403+carolyxn@users.noreply.github.com>
2022-08-30 19:44:36 -0700

    Add files via upload

---
+++ assignment2-1.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int miles; 
+    int balance; 
+    int days; 
+
+    miles = 4276;
+    balance = -20;
+    days = 189000;
+
+
+    cout << "We have made a long journey of "<< miles << "miles." << endl;
+    cout << "Our checking account balance is " << balance << "." << endl;
+    cout << "About " << days << "days ago Columbus stood on this spot." << endl;
+}
